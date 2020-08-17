a5807a40951587f8f7aeca6b1c1161f619ca3913
Van Tu Trieu 97 <vantutrieu97@gmail.com>
2020-08-17 23:58:26 +0700

    Custom Toast

--- app/src/main/java/vantutrieu97/xyz/googleandroidcertification/MainActivity.kt
+++ app/src/main/java/vantutrieu97/xyz/googleandroidcertification/MainActivity.kt
@@ -1,12 +1,29 @@
 package vantutrieu97.xyz.googleandroidcertification
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inflater = layoutInflater
+        val container: ViewGroup? = findViewById(R.id.custom_toast_container)
+        val layout: ViewGroup = inflater.inflate(R.layout.custom_toast, container) as ViewGroup
+        val text: TextView = layout.findViewById(R.id.text)
+        text.text = "This is a custom toast"
+        with(Toast(applicationContext)) {
+            setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+            duration = Toast.LENGTH_LONG
+            view = layout
+            show()
+        }
     }
 }
