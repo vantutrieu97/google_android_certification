45a31608ff1eb27a7bd7d67a2e1779a957142329
Van Tu Trieu 97 <vantutrieu97@gmail.com>
2020-08-19 00:58:32 +0700

    Snackbar

--- app/src/main/java/vantutrieu97/xyz/googleandroidcertification/MainActivity.kt
+++ app/src/main/java/vantutrieu97/xyz/googleandroidcertification/MainActivity.kt
@@ -1,18 +1,25 @@
 package vantutrieu97.xyz.googleandroidcertification
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import vantutrieu97.xyz.googleandroidcertification.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var mContext: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        mContext = this
 
         val inflater = layoutInflater
         val container: ViewGroup? = findViewById(R.id.custom_toast_container)
@@ -25,5 +32,19 @@ class MainActivity : AppCompatActivity() {
             view = layout
             show()
         }
+        val snackbar = Snackbar.make(
+            ct_wrap_view, "This is simple snackbar", Snackbar.LENGTH_INDEFINITE
+        ).setAction("Action", View.OnClickListener {
+            Toast.makeText(mContext, "click to snackbar", Toast.LENGTH_SHORT).show()
+        })
+
+        snackbar.setActionTextColor(Color.WHITE)
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(Color.BLACK)
+        val textView =
+            snackbarView.findViewById(R.id.snackbar_text) as TextView
+        textView.setTextColor(Color.WHITE)
+        textView.textSize = 12f
+        snackbar.show()
     }
 }
